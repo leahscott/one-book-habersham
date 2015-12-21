916ce2d34eff13cd8609b3a4274595dfcc931849
liamscott <lcscott123@gmail.com>
2015-12-21 00:17:54 -0500

    Add twitter feed

---
+++ js/site.js
@@ -0,0 +1,13 @@
+var config = {
+  "id": '678795546287579142',
+  "domId": 'twitter-container',
+  "maxTweets": 3,
+  "enableLinks": true,
+  "showUser": false,
+  "showTime": false,
+  "showImages": false,
+  "showRetweet": false,
+  "showInteraction": false
+};
+
+twitterFetcher.fetch(config);
